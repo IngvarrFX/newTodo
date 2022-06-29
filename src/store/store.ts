cf2f06c95c25c6795a8456882723a6e117a1b02b
IngvarrFX <nurov.mj@gmail.com>
2022-06-29 22:03:09 +0300

    add app reducer and middleware thunk

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,17 +3,21 @@ import thunk from "redux-thunk";
 import {taskReducer, todolistReducer} from "./reducers";
 import {TodolistActionsType} from "./reducers/todolistReducer";
 import {TaskActionsType} from "./reducers/taskReducer";
+import {appReducer} from "./reducers/appReducer";
 
 const rootReducer = combineReducers({
+    app: appReducer,
     tasks: taskReducer,
     todolists: todolistReducer,
 });
 
-//export const store = createStore(rootReducer);
+export const store = createStore(rootReducer,  applyMiddleware(thunk));
 
+/*
 export const store: Store<AppStateType, AppActionType> & {
     dispatch: DispatchType
 } = createStore(rootReducer, applyMiddleware(thunk))
+*/
 
 
 export type AppStateType = ReturnType<typeof rootReducer>;
