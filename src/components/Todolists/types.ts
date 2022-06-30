96193152d3df8b1bcf5e3d3861283c06807edaf9
IngvarrFX <nurov.mj@gmail.com>
2022-06-30 16:53:17 +0300

    Transferred the types from App to a separate type file

---
+++ src/components/Todolists/types.ts
@@ -0,0 +1,27 @@
+export type TaskType = {
+    description: string
+    title: string
+    completed: boolean
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+    id: string
+    todoListId: string
+    order: number
+    addedDate: string
+}
+
+export type TasksType = {
+    [key: string]: Array<TaskType>
+}
+
+export type TodoType = {
+    id: string
+    title: string
+    filter: FilterType
+    addedDate: string
+    order: number
+}
+
+export type FilterType = "All" | "Completed" | "Active";
