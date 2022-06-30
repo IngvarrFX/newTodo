ee54493594c9c77ebbeb4d77360c9885f82ac7ce
IngvarrFX <nurov.mj@gmail.com>
2022-06-30 16:51:24 +0300

    add setTodolists AC

--- src/store/actions/todoActions.ts
+++ src/store/actions/todoActions.ts
@@ -1,20 +1,29 @@
-import {AddTodolistType, ChangeTodolistFilterType, ChangeTodolistTitleType, RemoveTodolistType} from "./actionsTypes";
-import {FilterType} from "../../App";
+import {
+    AddTodolistType,
+    ChangeTodolistFilterType,
+    ChangeTodolistTitleType,
+    RemoveTodolistType,
+    SetTodolistsType
+} from "./actionsTypes";
+import {FilterType, TodoType} from "../../components/Todolists/types";
 
+export const setTodolistsAC = (todolists: any): SetTodolistsType => {
+    return {type: "SET_TODOLISTS", todolists}
+};
 
-export const addTodolistAC = (todoId: string, title: string): AddTodolistType => {
-    return {type: "ADD_TODOLIST", todoId, title}
+export const addTodolistAC = (todoId: string, newTodolist: TodoType): AddTodolistType => {
+    return {type: "ADD_TODOLIST", todoId, newTodolist}
 };
 
 export const removeTodolistAC = (todoId: string): RemoveTodolistType => {
     return {type: "REMOVE_TODOLIST", todoId}
 };
 
-export const changeTodolistFilterAC = (todoId: string, filter:FilterType): ChangeTodolistFilterType => {
+export const changeTodolistFilterAC = (todoId: string, filter: FilterType): ChangeTodolistFilterType => {
     return {type: "CHANGE_TODOLIST_FILTER", todoId, filter}
 };
 
-export const changeTodolistTitleAC = (todoId: string, title:string): ChangeTodolistTitleType => {
+export const changeTodolistTitleAC = (todoId: string, title: string): ChangeTodolistTitleType => {
     return {type: "CHANGE_TODOLIST_TITLE", todoId, title}
 };
 
