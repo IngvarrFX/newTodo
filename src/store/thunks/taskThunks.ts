791d195f08c0758cba122bdda334e79717afacfb
IngvarrFX <nurov.mj@gmail.com>
2022-06-30 16:57:05 +0300

    create thunks for tasks

---
+++ src/store/thunks/taskThunks.ts
@@ -0,0 +1,38 @@
+import {AnyAction} from "redux"
+import {ThunkAction} from "redux-thunk"
+import {AppStateType} from "../store";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, setTasksAC} from "../actions/tasksActions";
+import {tasksApi} from "../../api/todolists-api";
+
+export const getTasksTC = (todoId: string): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
+    tasksApi.getTasks(todoId).then((tasks) => {
+        dispatch(setTasksAC(todoId, tasks))
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+export const createTaskTC = (todoId: string, title: string): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
+    tasksApi.createTask(todoId, title).then((tasks) => {
+        dispatch(addTaskAC(todoId, tasks.data.item.id, title))
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+export const updateTaskTC = (todoId: string, taskId: string, title: string, status: number): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
+    tasksApi.updateTask(todoId, taskId, title, status).then((tasks) => {
+        dispatch(changeTaskTitleAC(todoId, taskId, title))
+        dispatch(changeTaskStatusAC(todoId, taskId, status))
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+
+export const removeTaskTC = (todoId: string, taskId: string): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
+    tasksApi.deleteTask(todoId, taskId).then((tasks) => {
+        dispatch(removeTaskAC(todoId, taskId))
+    }).catch((err) => {
+        console.log(err)
+    })
+}
