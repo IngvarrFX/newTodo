9197e3bb99b5a59160f94a22d7507480ac3cd0b3
IngvarrFX <nurov.mj@gmail.com>
2022-06-30 16:54:39 +0300

    create taskAPI

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {ResponseType, ResponseDataType, CreateResponseType} from "./types";
+import {ResponseDataType} from "./types";
 
 const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.1/",
@@ -12,15 +12,40 @@ const instance = axios.create({
 
 export const todolistsApi = {
     getTodolists: () => {
-        return instance.get<ResponseType>(`todo-lists`).then(res => res.data)
+        return instance.get(`todo-lists`).then(res => res.data.map((todo: any) => ({...todo, filter: "All"})))
     },
     createTodolist: (title: string) => {
-        return instance.post<ResponseDataType<CreateResponseType>>(`todo-lists`, {title}).then(res => res.data)
+        return instance.post(`todo-lists`, {title}).then(res => res.data.data.item)
     },
     updateTodolist: (id: string, title: string) => {
-        return instance.put<ResponseDataType>(`todo-lists/${id}`, {title}).then(res => res.data)
+        return instance.put(`todo-lists/${id}`, {title}).then(res => res.data)
     },
     deleteTodolist: (id: string) => {
         return instance.delete<ResponseDataType>(`todo-lists/${id}`).then(res => res.data)
+    },
+
+}
+
+export const tasksApi = {
+    getTasks: (todoId: string) => {
+        return instance.get(`todo-lists/${todoId}/tasks`).then(res => res.data.items)
+    },
+    createTask: (todoId: string, title: string) => {
+        return instance.post(`todo-lists/${todoId}/tasks`, {title}).then(res => res.data.items)
+    },
+    updateTask: (todoId: string, taskId: string, title: string, status: number) => {
+        const data = {
+            title,
+            completed: false,
+            description: "",
+            status,
+            priority: 0,
+            startDate: "",
+            deadline: "",
+        }
+        return instance.put(`todo-lists/${todoId}/tasks/${taskId}`, data).then(res => res.data.items)
+    },
+    deleteTask: (todoId: string, taskId: string) => {
+        return instance.delete(`todo-lists/${todoId}/tasks/${taskId}`).then(res => res.data.items)
     },
 }
