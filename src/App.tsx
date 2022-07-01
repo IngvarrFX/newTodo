65df76713cf57e77ffa5e3a03b357cd2bc4bbdd5
IngvarrFX <nurov.mj@gmail.com>
2022-07-01 11:55:56 +0300

    add preloader when initializing in progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import "./App.css";
 import {Todolists} from "./components/Todolists";
 import {useSelector} from "react-redux";
 import {AppStateType} from "./store/store";
+import {Preloader} from "./components/Preloader";
 
 function App() {
 
     const initialize = useSelector<AppStateType, boolean>(state => state.app.initializing);
 
-    if(initialize){
-        return <div>Loading Todolists...</div>
+    if (initialize) {
+        return <div style={{top: "50%", left:"50%"}}><Preloader/></div>
     }
 
     return (<div className="App">
