65e6c4f09c5c40a03d5d285c063d323ed24f9391
IngvarrFX <nurov.mj@gmail.com>
2022-07-07 17:26:27 +0300

    refactored style code

--- src/store/thunks/taskThunks.ts
+++ src/store/thunks/taskThunks.ts
@@ -5,24 +5,26 @@ import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, setTasks
 import {tasksApi} from "../../api/todolists-api";
 import {TaskType} from "../../components/Todolists/types";
 
-export const getTasksTC = (todoId: string): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
-    tasksApi.getTasks(todoId).then((tasks) => {
-        dispatch(setTasksAC(todoId, tasks))
-    }).catch((err) => {
-        console.log(err)
-    })
-}
+export const getTasksTC = (todoId: string): ThunkAction<void, AppStateType, unknown, AnyAction> =>
+    (dispatch) => {
+        tasksApi.getTasks(todoId).then((tasks) => {
+            dispatch(setTasksAC(todoId, tasks))
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
 
-export const createTaskTC = (todoId: string, title: string): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
-    tasksApi.createTask(todoId, title).then((task:TaskType) => {
-        dispatch(addTaskAC(task))
-    }).catch((err) => {
-        console.log(err)
-    })
-}
+export const createTaskTC = (todoId: string, title: string): ThunkAction<void, AppStateType, unknown, AnyAction> =>
+    (dispatch) => {
+        tasksApi.createTask(todoId, title).then((task: TaskType) => {
+            dispatch(addTaskAC(task))
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
 
 export const updateTaskTC = (todoId: string, taskId: string, title: string, status: number): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
-    tasksApi.updateTask(todoId, taskId, title, status).then((tasks) => {
+    tasksApi.updateTask(todoId, taskId, title, status).then(() => {
         dispatch(changeTaskTitleAC(todoId, taskId, title))
         dispatch(changeTaskStatusAC(todoId, taskId, status))
     }).catch((err) => {
@@ -30,10 +32,11 @@ export const updateTaskTC = (todoId: string, taskId: string, title: string, stat
     })
 }
 
-export const removeTaskTC = (todoId: string, taskId: string): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
-    tasksApi.deleteTask(todoId, taskId).then((tasks) => {
-        dispatch(removeTaskAC(todoId, taskId))
-    }).catch((err) => {
-        console.log(err)
-    })
-}
+export const removeTaskTC = (todoId: string, taskId: string): ThunkAction<void, AppStateType, unknown, AnyAction> =>
+    (dispatch) => {
+        tasksApi.deleteTask(todoId, taskId).then(() => {
+            dispatch(removeTaskAC(todoId, taskId))
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
