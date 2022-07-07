c475c56029166e86b742b814b21848d892939d90
IngvarrFX <nurov.mj@gmail.com>
2022-07-07 17:24:41 +0300

    replace hook useSelector on useAppSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
 import {Todolists} from "./components/Todolists";
-import {useSelector} from "react-redux";
-import {AppStateType} from "./store/store";
 import {Preloader} from "./components/Preloader";
+import {useAppSelector} from "./hooks";
 
 function App() {
 
-    const initialize = useSelector<AppStateType, boolean>(state => state.app.initializing);
+    const initialize = useAppSelector(state => state.app.initializing);
 
     if (initialize) {
         return <div style={{top: "50%", left:"50%"}}><Preloader/></div>
