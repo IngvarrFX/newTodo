b8761df0dad95e382e241c837a833e22dccaaea0
IngvarrFX <nurov.mj@gmail.com>
2022-07-12 11:10:56 +0300

    refactored types

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,9 @@
 import {applyMiddleware, combineReducers, createStore, Store} from "redux";
-import thunk from "redux-thunk";
+import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {appReducer, taskReducer, todolistReducer} from "./reducers";
 import {TodolistActionsType} from "./reducers/todolistReducer";
 import {TaskActionsType} from "./reducers/taskReducer";
+import {AppActionsType} from "./reducers/appReducer";
 
 const rootReducer = combineReducers({
     app: appReducer,
@@ -16,8 +17,12 @@ export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 //types
 export type AppStateType = ReturnType<typeof rootReducer>;
-export type AppDispatch = ReturnType<typeof store.dispatch>;
-/*export type AppActionType = TodolistActionsType | TaskActionsType;*/
+//appState type
+export type AppDispatch = ThunkDispatch<AppStateType, unknown, AppActionType>;
+//actions type
+export type AppActionType = AppActionsType | TodolistActionsType | TaskActionsType;
+//thunks type
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AppActionType>
 
 //@ts-ignore
 window.state = store.getState;
