7593eb6ee6a0d18053b31b2de857132fc1aa0e98
IngvarrFX <nurov.mj@gmail.com>
2022-07-12 11:10:34 +0300

    created hook useAppDispatch

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,4 +1,5 @@
-import {TypedUseSelectorHook, useSelector} from "react-redux";
-import {AppStateType} from "../store/store";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {AppDispatch, AppStateType} from "../store/store";
 
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector;
