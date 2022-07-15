cec3f3e2f86ea458672c9fcc32974740c02efe10
IngvarrFX <nurov.mj@gmail.com>
2022-07-15 20:19:54 +0300

    refactored size

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -3,7 +3,7 @@ import {Box, CircularProgress} from "@material-ui/core";
 
 export const Preloader = () => {
     return (<Box sx={{display: "flex"}}>
-            <CircularProgress size={"30px"}/>
+            <CircularProgress size={"70px"}/>
         </Box>
     );
 };
